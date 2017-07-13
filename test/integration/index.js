49e53aa858b1aee5efc82711108c2dfc3f1b7608
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-07-13 14:48:02 -0400

    Lock Firefox and watch test

--- test/integration/index.js
+++ test/integration/index.js
@@ -4,8 +4,10 @@ var execa = require('execa');
 
 test('Grunt task works', function (t) {
 	var gruntfilePath = path.join(__dirname, 'Gruntfile.js');
+	console.log('<Integration>')
 	return execa('grunt', ['--gruntfile', gruntfilePath, 'testee'])
 		.then(function () {
 			t.pass();
+			console.log('</Integration>')
 		});
 });
